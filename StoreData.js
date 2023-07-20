e3e66f613a4a6b27830ae8dfc6bbe6d08a9de1d0
Aayush Gupta <guptaaayush432@gmail.com>
2023-07-20 20:43:07 +0530

    [script] removed unnecessary comments

--- StoreData.js
+++ StoreData.js
@@ -12,15 +12,10 @@ const StoreData = async () => {
 
   const contract = new ethers.Contract(contractAddress, aggregatorAbi, signer);
 
-  // Id 1
-  // const fileLink = "baga6ea4seaqdyplmndx33rub426i74muaroivm5lnebayap2hon7lfakpkgeici";
+  // Transcation detail
   const fileLink = "https://gateway.lighthouse.storage/ipfs/bafkreia4ruswe7ghckleh3lmpujo5asrnd7hrtu5r23zjk2robpcoend34";
   const fileLinkBytes = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(fileLink));
-  // const fileSize = 512;
-
-  // Id 2
-  // const fileLinkBytes = "0x0181e2039220200d0e0a0100030000000000000000000000000000000000000000000000000000";
-  // const fileSize = 536870912;
+  
 
   let StoreData = await contract.StoreData(fileLinkBytes, {
     gasLimit: 50_000_000,
@@ -29,13 +24,5 @@ const StoreData = async () => {
   console.log(StoreData);
   console.log("transaction done");
 };
-
-// Call the addPlan function and catch if there is any error
-// StoreData()
-//   .then(() => process.exit(0))
-//   .catch(error => {
-//     console.error(error);
-//     process.exit(1);
-//   });
 
 module.exports = StoreData;
